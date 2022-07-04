fa12675c8ebf52b6ae3013991e50cc3398590612
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2022-07-04 14:59:40 +0200

    deleted gif.js because of already integrated command + message answer fixed

--- messages/answer.js
+++ messages/answer.js
@@ -1,8 +1,10 @@
 async function answerBack(message, data){
-    if(message.content.includes("quoi")){
-        message.channel.send(`<@${message.author.id}> ${data["quoi"][Math.floor(Math.random()*data["quoi"].length)]}`);
-    } else if (message.content.includes("oui")){
-        message.channel.send(`<@${message.author.id}> ${data["oui"][Math.floor(Math.random()*data["oui"].length)]}`);
+    if(!message.author.bot){
+        if(message.content.includes("quoi")){
+            message.channel.send(`<@${message.author.id}> ${data["quoi"][Math.floor(Math.random()*data["quoi"].length)]}`);
+        } else if (message.content.includes("oui")){
+            message.channel.send(`<@${message.author.id}> ${data["oui"][Math.floor(Math.random()*data["oui"].length)]}`);
+        }
     }
 }
 
