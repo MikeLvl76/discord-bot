aeec9b330b4ac6b65faa0494548495ebe5714763
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2022-07-07 23:13:00 +0200

    personalized command for weather

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 const { token, answers, channelId, todo} = require('./config.json');
 const handleCommand = require('./handles/command');
 const answerBack = require('./messages/answer');
-const wiki = require('./messages/command');
+const {wiki, weather} = require('./messages/command');
 
 client.commands = new Collection();
 
@@ -19,13 +19,13 @@ for (const file of files) {
 
 client.once('ready', () => {
     console.log("Ready to interact !");
-    const check = 1000 * 60;
+    const check = 1000 * 300; // in ms
     setInterval(() => {
         client.channels.fetch(channelId[0])
             .then(channel => channel.send(todo[Math.floor(Math.random()*todo.length)]));
         console.log(`[${client.user.username}] at [${new Date().toLocaleString()}] send a message`);
     }, check);
-    const scheduledMessage = new cron.CronJob('00 26 11 * * *', () => {
+    const scheduledMessage = new cron.CronJob('00 00 21 * * *', () => {
         client.channels.fetch(channelId[0])
             .then(channel => channel.send(`I'M HERE`));
     });
@@ -40,6 +40,7 @@ client.on('interactionCreate', async interaction => {
 client.on('messageCreate', message => {
     answerBack(message, answers);
     wiki(message);
+    weather(message);
 });
 
 client.login(token);
--- messages/command.js
+++ messages/command.js
@@ -1,40 +1,70 @@
-SPECIAL_COMMANDS = ['$wiki']
+const puppeteer = require('puppeteer');
+
+SPECIAL_COMMANDS = ['$wiki', '$weather']
 LANG = ['fr', 'en', 'de', 'it']
 
 function wiki(message) {
-    if(!message.author.bot){
+    if (!message.author.bot) {
         if (message.content.startsWith(SPECIAL_COMMANDS[0])) {
-            parts = message.content.split(' ');
-            url = '';
+            const parts = message.content.split(' ');
+            const url = '';
             switch (parts[1]) {
                 case 'info':
                     message.channel.send(`Use $wiki for a Wikipedia link\nFormat must be "$wiki [lang] [message]".\nLanguages available : ${LANG}`);
                     break;
-    
+
                 case 'fr':
                     url = "https://fr.wikipedia.org/wiki/" + parts[2];
                     break;
-    
+
                 case 'en':
                     url = "https://en.wikipedia.org/wiki/" + parts[2];
                     break;
-    
+
                 case 'de':
                     url = "https://de.wikipedia.org/wiki/" + parts[2];
                     break;
-    
+
                 case 'it':
                     url = "https://it.wikipedia.org/wiki/" + parts[2];
                     break;
-    
+
                 default:
                     break;
             }
             message.channel.send(url !== '' ? url : 'Nothing to send anymore.');
+        }
+    }
+}
+
+function weather(message) {
+    if (!message.author.bot) {
+        if (message.content.startsWith(SPECIAL_COMMANDS[1])) {
+            let i = message.content.indexOf(' ');
+            let parts = [message.content.slice(0, i), message.content.slice(i + 1)]; // split only once by space character
+            if (parts[1] === 'info') {
+                message.channel.send("Use $weather to see what's forecasted for your city.\nFormat must be $weather [city].\n");
+            } else {
+                while (parts[1].includes(' ')) {
+                    parts[1] = parts[1].replace(' ', '-');
+                }
+            }
+            const url = `https://fr.weather-forecast.com/locations/${parts[1]}/forecasts/latest`;
+            puppeteer.launch().then(async browser => {
+                const page = await browser.newPage()
+                await page.goto(url)
+                const temperatures = await page.evaluate(
+                    () => Array.from(
+                        document.querySelectorAll('td[class="b-forecast__table-cell-chill b-forecast__table-day-end"]'),
+                        elt => elt.textContent
+                    )
+                );
+                message.channel.send(`Felt temperature of ${parts[1]} today : ${temperatures[0]}°C.\nFelt temperature for next 3 days : ${temperatures.slice(1, 4)}`);
+            });
         } else {
             message.channel.send("Unknown command, please retry.");
         }
     }
 }
 
-module.exports = wiki;
+module.exports = {wiki, weather};
